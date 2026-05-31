f1639a315592a26df0d1b383bef96f901fee60b6
veveestudos92-art <veve.estudos92@gmail.com>
2026-05-31 16:13:49 -0300

    Fix event default prevention in form submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const senha = document.getElementById("senha");
 const mensagem = document.getElementById("mensagem");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault(); // evita o recarregamento da página
+    e.preventDefault(); 
     if (usuario.value === "" || senha.value === "") {
         mensagem.textContent = "Preencha todos os campos!";
     } else if (usuario.value === "adm" && senha.value === "123") {
